539960b0b3f2719061a60d89b803087c99b0e385
glo1214 <glo791214@gmail.com>
2026-06-16 18:04:48 +0900

    main.jsx 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-괜찮아요! 아래 내용 전체를 붙여넣으세요.
-jsximport '@tabler/icons-webfont/dist/tabler-icons.css'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import '@tabler/icons-webfont/dist/tabler-icons.css' 
+import { StrictMode } from 'react' 
+import { createRoot } from 'react-dom/client' 
+import './index.css' 
+import App from './App.jsx' 
+ 
+createRoot(document.getElementById('root')).render( 
+  <StrictMode> 
+    <App /> 
+  </StrictMode>, 
