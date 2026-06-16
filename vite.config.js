df38da41a89e5829d67bdda16435e85bff20424c
glo1214 <glo791214@gmail.com>
2026-06-16 17:56:41 +0900

    vite 설정 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-jsimport { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/ddakiji-hanja/',
-})
+import { defineConfig } from 'vite' 
+import react from '@vitejs/plugin-react' 
+ 
+export default defineConfig({ 
+  plugins: [react()], 
+  base: '/ddakiji-hanja/', 
+}) 
