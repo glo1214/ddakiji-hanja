0f9b729df9bb04e7b864d7daef80c9dbff29f35e
glo1214 <glo791214@gmail.com>
2026-06-16 17:22:29 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+괜찮아요! 아래 내용 전체를 붙여넣으세요.
+jsximport '@tabler/icons-webfont/dist/tabler-icons.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
